9d810ad877cb0bbb7bc14d6b10a26e926ddb83b0
destroierdam <33325102+destroierdam@users.noreply.github.com>
2017-11-02 18:35:11 +0200

    Create Task 1.cpp

---
+++ IS/UP/HW/1/Task 1.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+#include<cmath>
+using namespace std;
+int main()
+{
+    int n,i,sum=0,t;
+    cin>>n;
+    for(i=0;i<n;i++)
+    {
+        cin>>t;
+        sum+=t;
+    }
+    cout<<sum<<endl;
+    return 0;
+}
